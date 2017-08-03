2b5cf9f4548bc9ebad843788f22c8c9a39ad9784
alex dyakonov <alexdyakonov@3467473.no-reply.drupal.org>
2017-08-03 16:28:46 +0300

    Add WC sort products by title

--- wp-content/themes/handmade-child/functions.php
+++ wp-content/themes/handmade-child/functions.php
@@ -14,3 +14,26 @@ function g5plus_child_theme_setup(){
 
 // Include Widgets and Sidebars.
 require_once( get_stylesheet_directory() . '/inc/widgets-sidebars.php' );
+
+add_filter( 'woocommerce_get_catalog_ordering_args', 'custom_woocommerce_get_catalog_ordering_args' );
+function custom_woocommerce_get_catalog_ordering_args( $args ) {
+
+	$orderby_value = isset( $_GET['orderby'] ) ? wc_clean( $_GET['orderby'] ) : apply_filters( 'woocommerce_default_catalog_orderby', get_option( 'woocommerce_default_catalog_orderby' ) );
+
+	if ( 'name_list' == $orderby_value ) {
+		$args['orderby'] = 'title';
+		$args['order'] = 'ASC';
+		$args['meta_key'] = '';
+	}
+
+	return $args;
+
+}
+
+add_filter( 'woocommerce_default_catalog_orderby_options', 'custom_woocommerce_catalog_orderby' );
+add_filter( 'woocommerce_catalog_orderby', 'custom_woocommerce_catalog_orderby' );
+
+function custom_woocommerce_catalog_orderby( $sortby ) {
+	$sortby['name_list'] = 'По имени';
+	return $sortby;
+}
