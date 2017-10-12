63ed9abf81a5ac376567f6ab0234a1eb7d75ed5d
alex dyakonov <alexdyakonov@3467473.no-reply.drupal.org>
2017-10-12 17:57:07 +0300

    Collection and Recommended

--- wp-content/themes/handmade-child/template-collections.php
+++ wp-content/themes/handmade-child/template-collections.php
@@ -2,17 +2,38 @@
 /*
 * Template Name: Collections Template
 */
-?>
+
 // Доделать пагинацию.
-<?php get_header(); ?>
+?>
+<?php get_header();
+
+if ( get_query_var( 'paged' ) ){
+	$paged = get_query_var('paged');
+}
+else if ( get_query_var( 'page' ) ){
+	$paged = get_query_var( 'page' );
+}
+else {
+	$paged = 1;
+}
+$per_page = 12;
+$number_of_pages = count( get_terms( 'collections',array('hide_empty'=>'0') ) );
+$offset = $per_page * ( $paged - 1) ;
+
+// Setup the arguments to pass in
+$args = array(
+    'offset'     => $offset,
+    'number'     => $per_page,
+    'hide_empty' =>'0'
+);
+
+    // Gather the series
+$terms = get_terms( 'collections', $args );
+?>
 <div class="container clearfix">
     <div class="row clearfix">
 <?php
-$args  = array(
-	'taxonomy'   => 'collections',
-	'hide_empty' => TRUE,
-);
-$terms = get_terms( $args );
+// Loop through and display the series
 if ( ! empty( $terms ) ) {
 	echo '<div class="col-md-9">';
 	echo '<div class="clearfix layout-container">';
@@ -24,13 +45,22 @@ if ( ! empty( $terms ) ) {
 		?>
         <div class="col-md-4 collection-item">
             <a href="<?php echo get_term_link( (int)$term->term_id, 'collections' ); ?>"><img src="<?php echo $img; ?>" width="300"
-                                       height="300" border="0"
-                                       alt="<?php echo $term->name; ?>"></a>
+                                                                                              height="300" border="0"
+                                                                                              alt="<?php echo $term->name; ?>"></a>
         </div>
 		<?php
 	}
 	echo '</div></div></div>';
 }
+$big = 999999;
+echo '<div class="collection-pagination">';
+echo paginate_links( array(
+	'base'    => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+	'format'  => '?paged=%#%',
+	'current' => $paged,
+	'total'   => ceil( $number_of_pages / $per_page )
+) );
+echo '</div>';
 ?>
     </div>
 </div>
